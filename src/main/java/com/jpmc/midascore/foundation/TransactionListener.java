16d93b5f4f79a082409d39664c80aeffbccc743f
milly-says <millymaan11@gmail.com>
2026-06-08 09:24:43 +0530

    Task 2: Added kafka listener for transactions

---
+++ src/main/java/com/jpmc/midascore/foundation/TransactionListener.java
@@ -0,0 +1,15 @@
+package com.jpmc.midascore.foundation;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void listen(Transaction transaction) {
+        System.out.println("Received transaction: " + transaction);
+        System.out.println("Amount: " + transaction.getAmount());
+    }
+
+}
